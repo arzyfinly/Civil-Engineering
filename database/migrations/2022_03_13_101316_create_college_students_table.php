2669c4e994d4305078e1134602ec8ca29d599456
AndyYulianto11 <andyyulianto40@gmail.com>
2022-03-14 16:02:08 +0700

    update views

---
+++ database/migrations/2022_03_13_101316_create_college_students_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCollegeStudentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('college_students', function (Blueprint $table) {
+            $table->id();
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('surename');
+            $table->foreignId('user_id')->constrained('users');
+            $table->string('nim');
+            $table->string('alamat');
+            $table->date('tgl_lahir');
+            $table->string('tmpt_lahir');
+            $table->string('no_hp');
+            $table->string('gender');
+            $table->string('photo')->nullable();
+            $table->string('kelas')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('college_students');
+    }
+}
